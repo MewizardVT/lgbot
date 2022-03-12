9f917baabc91c0277cbb061bbb1d19af28d82afb
MewizardVT <84827662+MewizardVT@users.noreply.github.com>
2022-03-12 11:13:47 -0500

    cleanup code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bot = new Discord.Client() // bot declaration
 const fs = require('fs'); // for if i decide to use it
 const { verify } = require("crypto"); // what the fuck is this
 
-
 bot.on("ready", async () => {
     console.log('on') // self explanitory
 })
@@ -25,9 +24,7 @@ bot.on("message", async message => {
 
     if(cmd === `${prefix}verify`) {
        
- let role = message.guild.roles.cache.get("952013157969330316"); // members
-
-
+let role = message.guild.roles.cache.get("952013157969330316"); // members
 let member = message.mentions.members.first(); // gets first pinged member
 
 if(message.member.roles.cache.has("952011804131872839")){ // owners
@@ -35,16 +32,11 @@ member.roles.add(role);
 } else {
     return message.reply("You do not have the required permissions.") // permission rejection
 }
-
 if(message.member.roles.cache.has("952027068818944050")){ // admins
   member.roles.add(role);
 } else {
     return message.reply("You do not have the required permissions.") // permission rejection
 }
-
-
-
     }
 })
-
 bot.login(config.token); // bot login
